046cd8a67a084ba8690e5f5c7a3670b765df9172
bae kyungtae <b841010@naver.com>
2024-07-06 03:23:01 +0900

    Blog app 테이블 migration

---
+++ blog/models.py
@@ -0,0 +1,31 @@
+from django.db import models
+from django.urls import reverse
+
+
+# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField('TITLE', max_length=200)
+    slug = models.SlugField('SLUG', unique=True, allow_unicode=True, help_text='one word for title alias.')
+    description = models.CharField('DESCRIPTION', max_length=100, blank=True, help_text='simple description test.')
+    content = models.TextField('CONTENT')
+    create_date = models.DateTimeField('CREATE DATE', auto_now_add=True)
+    modify_date = models.DateTimeField('MODIFY DATE', auto_now=True)
+
+    class Meta:
+        verbose_name = 'Post'
+        verbose_name_plural = 'Posts'
+        db_table = 'my_post'
+        ordering = ('-modify_date',)
+
+    def __str__(self):
+        return self.title
+
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', kwargs={'slug': self.slug, })
+
+    def get_previous_post(self):
+        return self.get_previous_by_modify_date()
+
+    def get_next_post(self):
+        return self.get_next_by_modify_date()
